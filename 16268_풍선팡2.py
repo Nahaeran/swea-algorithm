9522c035e47e0b0b1c381e2aaf45ec98166fbfc3
haeran <nh2545@naver.com>
2023-08-02 14:57:09 +0900

    solved: 16268 풍선팡2

---
+++ 16268_풍선팡2.py
@@ -0,0 +1,20 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    li = [list(map(int, input().split())) for _ in range(N)]
+
+    di = [1, 0, -1, 0, 0]
+    dj = [0, 1, 0, -1, 0]
+
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            temp = 0
+            for k in range(5):
+                ni = i + di[k]
+                nj = j + dj[k]
+                if 0 <= ni < N and 0 <= nj < M:
+                    temp += li[ni][nj]
+            if result < temp:
+                result = temp
+    print(f'#{tc} {result}')
