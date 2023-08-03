9724a1b0991a4d6669d060518cf0edf166eff705
haeran <nh2545@naver.com>
2023-08-03 16:16:59 +0900

    solved: 1961 숫자 배열 회전

---
+++ 1961_숫자_배열_회전.py
@@ -0,0 +1,20 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    li = [list(map(int, input().split())) for _ in range(n)]
+    li90 = list(map(list, zip(*li[::-1])))
+    li180 = list(map(list, zip(*li90[::-1])))
+    li270 = list(map(list, zip(*li180[::-1])))
+
+    print("#" + str(test_case))
+    for i in range(n):
+        result = ""
+        for j in range(n):
+            result += str(li90[i][j])
+        result += " "
+        for j in range(n):
+            result += str(li180[i][j])
+        result += " "
+        for j in range(n):
+            result += str(li270[i][j])
+        print(result)
