4e1d15cea3f87a1f70ba4d0668fa3f289b85d16a
nahaeran <nh2545@naver.com>
2023-09-11 11:12:43 +0900

    add: 연습문제1 퀵 정렬 연습

---
+++ 연습문제1_퀵_정렬_연습.py
@@ -0,0 +1,28 @@
+def quick_sort(low, high):
+    if high <= low:
+        return
+    mid = partition(low, high)
+    quick_sort(low, mid - 1)
+    quick_sort(mid, high)
+
+
+def partition(low, high):
+    pivot = arr[(low + high) // 2]
+    l, r = low, high
+
+    while l <= r:
+        while arr[l] < pivot:
+            l += 1
+        while arr[r] > pivot:
+            r -= 1
+        if l <= r:
+            arr[l], arr[r] = arr[r], arr[l]
+            l, r = l + 1, r - 1
+    return l
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    arr = list(map(int, input().split()))
+    quick_sort(0, len(arr) - 1)
+    print(f'#{tc}', *arr)
